a628e62d45e3f7e7eba6b2936c1ed30ad700b0c7
Julian Kizanis <julianzia@gmail.com>
2026-05-03 21:31:00 +0900

    removed old code

--- old_code/src/boost_and_broadside/modes/__init__.py
+++
@@ -1,9 +0,0 @@
-from .collect import collect
-from .play import play
-from .train import train
-
-__all__ = [
-    "collect",
-    "play",
-    "train",
-]
--- old_code/tests/components/test_state.py
+++
@@ -1,1 +0,0 @@
-# Test deleted as it targeted legacy env.state.State which no longer exists.
